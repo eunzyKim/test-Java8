379ae13520a485da1a2a484fbeae2531d015c408
HYERIN <hyerin@irm.kr>
2017-09-10 17:41:58 +0900

    - lamda test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,22 @@
 package com.company;
 
-public class Main {
+import java.security.cert.TrustAnchor;
+import java.util.Date;
 
+public class Main {
     public static void main(String[] args) {
-	    System.out.println("test");
-        System.out.println("꾸아");
+        /* old version */
+        new  Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Old version");
+            }
+        });
+
+        /* lamda */
+        Runnable runnable = () -> System.out.println("Runnable Functional Interface Variable");
+        new Thread(runnable).run();
+        new Thread(runnable).run();
+        new Thread(() -> System.out.println("Runnable Lamda Test")).run();
     }
 }
